b14ecc2353e90a76bfff01744b7734e4520e8930
greatYear <ywzhaiqi@gmail.com>
2017-08-31 17:01:04 +0800

    not insert __$styleInject function if codeCount = 0

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ function userScriptCss(options = {}) {
   const filter = createFilter(options.include || ['**/*.css'], options.exclude || 'node_modules/**')
 
   const injectFnName = '__$styleInject'
+  let codeCount = 0;
 
   return {
     name: 'userscriptCss',
     intro() {
-      return options.insert ? insertStyle.toString().replace(/insertStyle/, injectFnName) : '';
+      return options.insert && codeCount ? insertStyle.toString().replace(/insertStyle/, injectFnName) : '';
     },
 
     transform(code, id) {
@@ -22,6 +23,7 @@ function userScriptCss(options = {}) {
       let exportCode = '';
 
       if (options.insert != false) {
+        codeCount += 1;
         exportCode = `export default ${injectFnName}(${JSON.stringify(code)});`;
       } else {
         exportCode = `export default ${JSON.stringify(code.toString())};`
